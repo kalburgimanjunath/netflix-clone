871c5e0127fe3f1dd4360b7fb5b3b3afd16cd939
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-10 10:50:42 +0300

    added new email folder with email to be sent

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 import datetime as dt 
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request,'movies/index.html')
+@login_required(login_url='/accounts/login/')
+def movie_list(request):
